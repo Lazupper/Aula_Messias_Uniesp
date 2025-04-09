f0d5c379435dc5f59250cd75d3964bde4a6533bc
Lazup <LaurentAlvesMaia@gmail.com>
2025-04-09 20:11:39 -0300

    Update exercicio_09_1.py

--- exercicio_09_1.py
+++ exercicio_09_1.py
@@ -1,4 +1,16 @@
-# Loop para verificar os números entre 1000 e 2000
-for num in range(1000, 2001):
+import time
+
+# Modo rápido ou dramático?
+pausar = False  # Mude para True se quiser 1 segundo de intervalo entre os números
+
+for num in range(1000, 2004):
     if num % 11 == 5:
         print(num)
+        if pausar:
+            time.sleep(1)
+num = 1000
+
+while num <= 2003:
+    if num % 11 == 5:
+        print(num)
+    num += 1
