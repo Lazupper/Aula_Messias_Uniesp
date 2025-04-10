3c22cad31baefb26dd7a4864f6378d73fa32c616
Lazup <LaurentAlvesMaia@gmail.com>
2025-04-09 21:06:26 -0300

    Add files via upload

---
+++ Exercicio_16.py
@@ -0,0 +1,7 @@
+# Lista dos amiguinhos
+amigos = ["Xota", "Dieguinho", "Parrilha", "Noob Saibot", "Becca jeans", "johnny test"]
+
+# Exibindo com textinho
+print("Cada dia chego ao meu objetivo de 100 flamenguistas:\n")
+for amigo in amigos:
+    print(f"Mais um flamenguista adquirido com sucesso, prazer {amigo}")
